898f906490bb05b1171c74442011a86a7cd21f26
zadapp <45818651+zadapp@users.noreply.github.com>
2018-12-17 00:33:26 +0400

    Initial commit

---
+++ app/src/main/java/com/example/dell/zad/Rigestration.java
@@ -0,0 +1,13 @@
+package com.example.dell.zad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rigestration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Select);
+    }
+}
