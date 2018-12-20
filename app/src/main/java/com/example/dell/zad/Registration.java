28bc4e13e5acaf11090f35bd46b739d50834dc46
zadapp <45818651+zadapp@users.noreply.github.com>
2018-12-20 19:05:35 +0400

    Select to select add login

--- app/src/main/java/com/example/dell/zad/Registration.java
+++ app/src/main/java/com/example/dell/zad/Registration.java
@@ -3,11 +3,11 @@ package com.example.dell.zad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Rigestration extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_Select);
+        setContentView(R.layout.activity_registration);
     }
 }
